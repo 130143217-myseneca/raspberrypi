20110c8b09d44f353adf1f90c36f82f50b6d929c
130143217-myseneca <mmijares1@myseneca.ca>
2023-08-13 16:50:43 -0400

    Modified preprocessconditions

--- iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -231,8 +231,9 @@ latlong=lat:long'
 #     preprocesslogic='dataage_1_minute' # millisecond,second,minute,hour,day
 #     preprocesslogic='dataage_1_second' # millisecond,second,minute,hour,day
 #     preprocesslogic='dataage_1_millisecond' # millisecond,second,minute,hour,day
-
      
+     #Added a preprocess condition where Data has to be of integertype
+     preprocessconditions="PersonData.DataType=integer"
      #pathtotmlattrs='oem=id,lat=subject.reference,long=component.0.code.coding.0.display,location=component.1.valueQuantity.value'     
      pathtotmlattrs='oem=n/a,lat=n/a,long=n/a,location=n/a,identifier=n/a'     
      #pathtotmlattrs='oem=Person_ID,location=Location'
